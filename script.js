2c0ba9b6af067be19e5e1abd792f9d46cb22aee7
Speedster3030 <vonneumann87@gmail.com>
2026-02-27 16:19:15 +0530

    added click

--- script.js
+++ script.js
@@ -2,5 +2,11 @@
 
 document.addEventListener('click', function(event){
     const pixel= document.createElement('div');
-    pixel.
+    pixel.className= 'dot';
+
+    pixel.style.left= event.pageX + 'px';
+    pixel.style.top= event.pageY + 'px';
+
+    document.body.appendChild(pixel);
+});
 
