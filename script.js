80382e8f970185a3f299722c354299582e12e779
Speedster3030 <vonneumann87@gmail.com>
2026-02-27 16:31:17 +0530

    more neat attempt

--- script.js
+++ script.js
@@ -4,8 +4,10 @@ document.addEventListener('click', function(event){
     const pixel= document.createElement('div');
     pixel.className= 'dot';
 
-    let x= parseInt(event.pageX)/10;
-    let y=parseInt(event.pageY)/10;
+    let x= parseInt(event.pageX);
+    let y=parseInt(event.pageY);
+    x-=x%10;
+    y-=y%10;
 
     pixel.style.left= String(x) + 'px';
     pixel.style.top= String(y) + 'px';
